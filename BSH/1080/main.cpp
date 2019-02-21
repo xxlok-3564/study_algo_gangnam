fe40f4f486c68095e7f9886d95728bcba1298f12
BSH <baek9652@naver.com>
2019-02-21 23:36:56 +0900

    greedy

--- BSH/1080/main.cpp
+++ BSH/1080/main.cpp
@@ -2,21 +2,57 @@
 
 using namespace std;
 
+int row, col;
+
+void flip(bool board[50][50], bool ans[50][50], int x, int y) {
+
+    for (int new_y = y; new_y < y + 3; new_y++)
+        for (int new_x = x; new_x < x + 3; new_x++)
+            board[new_y][new_x] = !board[new_y][new_x];
+
+}
+
 int main() {
 
-    int row, col;
+
     cin >> row >> col;
 
-    bool board[row][col];
-    bool ans[row][col];
+    bool board[50][50];
+    bool ans[50][50];
+    char a;
+    int count = 0;
+
+    for (int y = 0; y < row; y++)
+        for (int x = 0; x < col; x++) {
+            cin >> a;
+            board[y][x] = a - '0';
+        }
+
+    for (int y = 0; y < row; y++)
+        for (int x = 0; x < col; x++) {
+            cin >> a;
+            ans[y][x] = a - '0';
+        }
+
+    for (int y = 0; y < row - 2; y++)
+        for (int x = 0; x < col - 2; x++)
+            if (board[y][x] != ans[y][x]) {
+                flip(board, ans, x, y);
+                count += 1;
+            }
+
+    int fail = 0;
 
     for (int y = 0; y < row; y++)
         for (int x = 0; x < col; x++)
-            cin >> board[y][x];
+            if (board[y][x] != ans[y][x])
+                fail = -1;
 
-    for (int y = 0; y < row; y++)
-        for (int x = 0; x < col; x++)
-            cin >> ans[y][x];
+    if (fail)
+        cout << fail;
+    else
+        cout << count;
+
 
     return 0;
 }
