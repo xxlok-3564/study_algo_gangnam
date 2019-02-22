61519b71bc1a97122e4f737c28b9ffa3f0cd4f13
BSH <baek9652@naver.com>
2019-02-22 10:59:11 +0900

    combination

--- BSH/combination/main.cpp
+++ BSH/combination/main.cpp
@@ -57,7 +57,26 @@ int f3(int n, int r, vector<int> &arr) {
 
 }
 
-void f4(){
+int idx_list[1000];
+
+void f4(int depth, int n, int c, int start_idx, vector<int> &arr) {
+
+    if (depth == c) {
+
+        for (int i = 0; i < c; i++)
+            cout << arr[idx_list[i]];
+
+        cout << endl;
+        return;
+    }
+
+    for (int i = start_idx; i < n; i++) {
+        idx_list[depth] = i;
+
+        f4(depth + 1, n, c, i + 1, arr);
+
+    }
+
 
 }
 
@@ -83,9 +102,11 @@ int main() {
 
     cout << "-------------------------" << endl;
 
-    cout << f3(n, c, arr) << endl;
+    //cout << f3(n, c, arr) << endl;
 
     cout << "-------------------------" << endl;
+
+    //f4(0, n, c, 0, arr);
 
 
     return 0;
