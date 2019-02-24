1ba4c90436d0b753c2bc28539143bd7432ceaa1c
BSH <baek9652@naver.com>
2019-02-25 08:40:12 +0900

    combination

---
+++ BSH/1005/cmake-build-debug/CMakeFiles/1005.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1005.dir/main.cpp.o"
+  "1005.pdb"
+  "1005"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1005.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
